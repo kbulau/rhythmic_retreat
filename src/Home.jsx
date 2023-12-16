5626c41a3acc07a8e75a5ff7c4c81c74b3d344a7
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-16 15:41:06 -0600

    style home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div className="grid grid-cols-12">
-        <div className="m-0 p-0 col-span-2 bg-blue-950 h-screen flex flex-col gap-4 pt-10 min-w-[150px]">
+        <div className="m-0 p-0 col-span-2 bg-slate-900 h-screen flex flex-col gap-4 pt-10 min-w-[150px]">
           <div className="text-slate-500 text-s">Browse</div>
           <div className="text-slate-500">music</div>
           <div className="text-slate-500">Stations</div>
@@ -13,15 +13,15 @@ export default function Home() {
           <div className="text-slate-500"> mv</div>
           <div className="text-slate-500 pb-4">Artist</div>
         </div>
-        <div className="col-span-10">
-          <header className="flex space-between flex-wrap px-10 h-16 content-center">
-            <div className="flex">hi</div>
-            <div className="flex gap-8">
+        <div className="col-span-10 bg-blue-950">
+          <header className="flex justify-between h-16 items-center border-slate-600 mx-16">
+            <div className="flex text-slate-400">hi</div>
+            <div className="flex gap-8 text-slate-400">
               <div>hi</div>
               <div>hi</div>
             </div>
           </header>
-          <div>noway</div>
+          <div className="text-slate-400">noway</div>
         </div>
       </div>
     </>
