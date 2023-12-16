d16eace9d34c79f447308ec87d0032e747fbad98
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-16 11:15:48 -0600

    set up base css files

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className=" border-stone-100 border-2"> hi</div>
+      <div>Rhythmic Retreat</div>
+      <div>
+        We use spotify to give you music suggestions, show you your tops
+        artists, and more! Just login below.
+      </div>
+      <div className=" border-stone-100 border-2">
+        {' '}
+        To access the app you must first login below
+      </div>
       <button className="bg-black">
         <a href="/api/token"> click me</a>
       </button>
