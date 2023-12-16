ad457bfd67dac2941ac4736c790c19addf2caab7
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-16 15:18:41 -0600

    add grid layout

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <div className="flex">
-        <div className="m-0 p-0 w-[200px] bg-blue-950 h-screen flex flex-col gap-4 pt-10">
+      <div className="grid grid-cols-12">
+        <div className="m-0 p-0 col-span-2 bg-blue-950 h-screen flex flex-col gap-4 pt-10 min-w-[150px]">
           <div className="text-slate-500 text-s">Browse</div>
           <div className="text-slate-500">music</div>
           <div className="text-slate-500">Stations</div>
@@ -13,8 +13,14 @@ export default function Home() {
           <div className="text-slate-500"> mv</div>
           <div className="text-slate-500 pb-4">Artist</div>
         </div>
-        <div>
-          <header>Hello</header>
+        <div className="col-span-10">
+          <header className="flex space-between flex-wrap px-10 h-16 content-center">
+            <div className="flex">hi</div>
+            <div className="flex gap-8">
+              <div>hi</div>
+              <div>hi</div>
+            </div>
+          </header>
           <div>noway</div>
         </div>
       </div>
