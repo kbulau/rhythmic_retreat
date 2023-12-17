8a8c4adc139dfb2ed8c010d1528e1ded4aaea3e7
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-16 19:02:08 -0600

    style sidebar hover state

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ export default function App() {
     <>
       <div className="background h-screen pt-20">
         <div className="bg-slate-400  bg-opacity-70 py-10 mx-96 rounded-3xl">
-          <div className="flex justify-center items-center gap-4 mb-[-20px]">
+          <div className="flex justify-center items-center gap-4 mb-[-20px] home">
             <i className="fa-solid fa-headphones fa-xl"></i>
             <h1 className="font-[sans]">Rhythmic Retreat</h1>
           </div>
 
           <div className="">
             <p className="font-[play] mb-4">
-              We use spotify to give you music suggestions, show you your top
+              We use Spotify to give you music suggestions, show you your top
               artists,
               <br />
               and more! Just login below.
--- src/Home.jsx
+++ src/Home.jsx
@@ -24,16 +24,26 @@ export default function Home() {
     <>
       <div className="grid grid-cols-12">
         <div className="m-0 p-0 col-span-2 bg-slate-900 h-screen flex flex-col gap-4 pt-10 min-w-[150px]">
-          <div className="flex gap-4 items-center justify-center pb-4">
+          <div className="flex gap-4 items-center justify-center pb-4 sidebar">
             <i className="fa-solid fa-headphones fa-sm"></i>
             <div className="text-slate-200 font-[sans]"> Rhythmic Retreat</div>
           </div>
 
-          <div className="text-slate-500 text-s">Top Artists</div>
-          <div className="text-slate-500">Top Tracks</div>
-          <div className="text-slate-500">Top Genres</div>
-          <div className="text-slate-500">Song Recs</div>
-          <div className="text-slate-500 mb-4">Random Songs</div>
+          <div className="text-slate-500 text-s sidebarOptions hover:text-green-600">
+            Top Artists
+          </div>
+          <div className="text-slate-500 sidebarOptions hover:text-green-600">
+            Top Tracks
+          </div>
+          <div className="text-slate-500 sidebarOptions hover:text-green-600">
+            Top Genres
+          </div>
+          <div className="text-slate-500 sidebarOptions hover:text-green-600">
+            Song Recs
+          </div>
+          <div className="text-slate-500 mb-4 sidebarOptions hover:text-green-600">
+            Random Songs
+          </div>
           <h3 className="text-slate-300 m-0 p-0 ">Playlists</h3>
           <div className="text-slate-500">Stations</div>
           <div className="text-slate-500"> mv</div>
