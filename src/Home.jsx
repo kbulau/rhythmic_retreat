ef0c60cdd4d38ec48a358adbe0c14628b9799f6b
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-19 17:32:58 -0600

    create state for variables

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,13 +3,37 @@ export default function Home() {
   const [user, setUser] = useState('');
   const [profilePic, setProfilePic] = useState('');
 
-  const [tracks, setTracks] = useState([]);
-  const [trackArt, setTrackArt] = useState([]);
-  const [trackImg, setTrackImg] = useState([]);
+  const [topArtists, setTopArtists] = useState([]);
+  const [topArtistImg, setTopArtistImg] = useState([]);
+  const [topGenres, setTopGenres] = useState([]);
+  const [topGenreData, setTopGenreData] = useState([]);
 
-  const [artists, setArtists] = useState([]);
-  const [artistImg, setArtistImg] = useState([]);
+  const [topTrackNames, setTopTrackNames] = useState([]);
+  const [topTrackArtists, setTopTrackArtists] = useState([]);
+  const [topTrackImgs, setTopTrackImgs] = useState([]);
 
+  const [featPlaylistName, setFeatPlaylistName] = useState([]);
+  const [featPlaylistImg, setFeatPlaylistImg] = useState([]);
+  const [featPlaylistHref, setFeatPlaylistHref] = useState([]);
+
+  const [newReleaseArtists, setNewReleaseArtists] = useState([]);
+  const [newReleaseAlbum, setNewReleaseAlbum] = useState([]);
+  const [newReleaseImgs, setNewReleaseImgs] = useState([]);
+  const [newReleaseHref, setNewReleaseHref] = useState([]);
+
+  const [hotHitArtists, setHotHitArtists] = useState([]);
+  const [hotHitAlbumImgs, setHotHitAlbumImgs] = useState([]);
+  const [hotHitTrackName, setHotHitTrackName] = useState([]);
+  const [hotHitPreview, setHotHitPreview] = useState([]);
+
+  const [relArtistImgs, setRelArtistImgs] = useState([]);
+  const [relArtistNames, setRelArtistNames] = useState([]);
+
+  const [songRecImg, setSongRecImg] = useState([]);
+  const [songRecArtistName, setSongRecArtistName] = useState([]);
+  const [songRecName, setSongRecName] = useState([]);
+
+  // separate useEffect just for profile since it will never change
   useEffect(() => {
     fetch('/api/profile').then((res) => {
       res.json().then((data) => {
